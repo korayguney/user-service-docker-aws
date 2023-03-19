c6f13d6edd9a469022bef6a256e5a0359340894b
Koray Güney <koray.guney@hotmail.com>
2023-03-19 20:10:38 +0300

    Initial commit

---
+++ src/main/java/com/test/userservice/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.test.userservice.repository;
+
+import com.test.userservice.entity.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Integer> {
+}
